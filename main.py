e3e8cfb8e34a2fbfedc90aced7e4f3d7657e1f05
Tim Wise <21957423@student.uwa.edu.au>
2018-08-24 23:01:19 +0800

    auto debug detection
    Added ability to call main.py with a debug argument and added corrresponding info to Setup.txt

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
+import sys
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 from tornado.web import RequestHandler
+from tornado.log import enable_pretty_logging
 
 class MainHandler(RequestHandler):
     def get(self):
         self.render("views/index.html")
 
 def main():
+    # Test if this module was called in debug mode, ie:
+    #   > python main.py debug
+    debug = len(sys.argv) > 1 and sys.argv[1].lower() == 'debug'
+    
+    if debug: enable_pretty_logging() # Log all GET, POST... requests
     application = tornado.web.Application(
         [(r"/", MainHandler),],
-        debug=False
+        debug=debug # Enable live changes to code
     )
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 5000))
