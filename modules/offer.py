35ff720fb6ecfc170d7f7229f0148e0f5ab30150
Jayden Kur <21988713@student.uwa.edu.au>
2018-09-10 15:15:49 +0800

    Try #2

--- modules/offer.py
+++ modules/offer.py
@@ -1,9 +1,9 @@
 class Player():
-    """Store and manage data about a single player"""
-    def __init__(self, player_id, is_seller):
-        self.player_id = player_id
-        self.ws = None
+    """Store and manage data about a single offer"""
+    def __init__(self, offer_id, type,  is_seller, price, time, player_id):
+        self.offer_id = offer_id
+        self.type = type
         self.is_seller = is_seller
-
-    def give_card(self, card):
-        self.card = card
+        self.price = price
+        self.time = time
+        self.player_id = player_id
