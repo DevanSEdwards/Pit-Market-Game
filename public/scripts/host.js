b01efb0b8e3dfa47677c9d4eb98a6a82b54cab2d
Tim Wise <21957423@student.uwa.edu.au>
2018-09-25 16:06:54 +0800

    Added state object to host

--- public/scripts/host.js
+++ public/scripts/host.js
@@ -1,8 +1,39 @@
-function loadpage(page)
-{
-    pages = document.getElementById('root').children;
-    pages.hidden = true;
-    document.getElementById(page + "Page").hidden = false;
+state = {
+    clientId: null,
+    gameId: null,
+    isHost: true,
+    currentPage: null,
+    deckSettings: {
+        domain: 7,
+        mean: 6,
+        lowerLimit: 2
+    },
+    offers: [],
+    rounds: [
+        // {
+        //     settings: {
+        //         length: 120,
+        //         offerTimeLimit: 10,
+        //         tax: null,
+        //         ceiling: null,
+        //         floor: null
+        //     },
+        //     trades: []
+        // }
+    ]
 }
 
-loadpage("round");
+function loadpage(page)
+{
+    pages = document.getElementById(`root`).children;
+    pages.hidden = true;
+    document.getElementById(`${page}Page`).hidden = false;
+}
+
+
+
+function main() {
+    loadpage(`deckSettings`);
+}
+
+document.onload = main;
