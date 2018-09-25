80c0d61eb4b57df4be741d373b36f867a2c246fe
thebagleboy <alfred.burgess95@gmail.com>
2018-09-25 13:29:02 +0800

    Offer class created. Now posts
    TODO: Connect to web sockets

--- views/offers.js
+++ views/offers.js
@@ -9,66 +9,81 @@ var _BUYER = false;
 
 class Offer
 {
-	constructor(id, buyer, time, value)
+	constructor(offer_id, buyer, val, time)
 	{
-		this.id = id;
+		this.offer_id = offer_id;
 		this.buyer = buyer;
+		this.val = val;
 		this.time = time;
-		this.value = value;
+		this.drawn = false;
 	}
-	get id() { return this.id; }
-	get time() { return this.time; }
-	get value() { return this.value; }
-	isBuyer() { return this.buyer; }
-	isSeller() { return !this.buyer; }
+	// Getters
+	offer_id() { return this.offer_id; }
+	buyer() { return this.buyer; }
+	//seller() { if(this.buyer){return false;} else {return true;} }
+	val() { return this.val; }
+	time() { return this.time; }
 
+	// Methods
 	drawHTML()
 	{
-		/* OFFER FORMAT
-          <div class='offer-shape buyer'>
-            <div class='offer-internal' style='float: left;'>
-              $ XX
-            </div>
-            <div class='offer-internal' style='float: right;'>
-              <button>Accept</button>
-            </div>
-          </div>
-		 */
-		var html = "<div class='offer-shape ";
-		if(isBuyer()) { html += "buyer"; }
-		html+="'><div class='offer-internal' style='float: left;'>$ ";
-		//html+=string(this.value);
-	html+=string(25);
-		html+="</div><div class='offer-internal' style='float: right;'>";
-		html+="<button ";
-		if(_BUYER == isBuyer()){ html += "disabled"; }
-		html+=">Accept</button></div></div>";
+	if(this.drawn) { /*Don't draw it again */ return 1; }
+	/* OFFER FORMAT
+      <div class='offer-shape buyer'>
+        <div class='offer-internal' style='float: left;'>
+          $ XX
+        </div>
+        <div class='offer-internal' style='float: right;'>
+          <button>Accept</button>
+        </div>
+      </div>
+	 */
+
+	// Create HTML element
+	var html = "<div class='offer-shape ";
+	if(this.isBuyer) { html += "buyer"; } else { html += "seller"; }
+	html+="'><div class='offer-internal' style='float: left;'>$ ";
+	html+=String(this.val);
+	html+="</div><div class='offer-internal' style='float: right;'>";
+	html+="<button ";
+	if(playerBuyer == false){ html += "disabled"; }
+	html+=">Accept</button></div></div>";
+
+	// Draw to screen
+	document.getElementById("offer-list").innerHTML+=html;
+
+	// Set as drawn
+	this.drawn = true;
+	return 0;
 	}
 }
 
-function draw()
-{
+function test_draw(offerBuyer, playerBuyer)
+{	/* Testing file for drawing offers */
 	var html = "<div class='offer-shape ";
-	if(false) { html += "buyer"; }
+	if(offerBuyer) { html += "buyer"; }
 	else { html += "seller"; }
 	html+="'><div class='offer-internal' style='float: left;'>$ ";
 	//html+=string(this.value);
 	html+=String(25);
 	html+="</div><div class='offer-internal' style='float: right;'>";
 	html+="<button ";
-	if(_BUYER == true){ html += "disabled"; }
+	if(playerBuyer == false){ html += "disabled"; }
 	html+=">Accept</button></div></div>";
 
-	document.getElementById("offer-list").innerHTML=html;
+	document.getElementById("offer-list").innerHTML+=html;
+	return 0;
 }
 
-function recieveNewOffer(offer)
+function recieveNewOffer(offer_id, buyer, val, time)
 {
-	lst_offers.push(offer);
-	reDrawOffers();
+	lst_offers.push(new Offer(offer_id, buyer, val, time))
 }
 
-function reDrawOffers()
+function drawOfferList()
 {
-	console.log("Redraw");
+	for(var i = 0; i < lst_offers.length; i++)
+	{
+		lst_offers[i].drawHTML();
+	}
 }
