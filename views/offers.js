8504d75c3f22f7195b1c134b603067bcc36d8832
thebagleboy <alfred.burgess95@gmail.com>
2018-09-25 12:44:57 +0800

    Combined buyer & seller offers draw() Now draws a single offer

--- views/offers.js
+++ views/offers.js
@@ -1,12 +1,11 @@
-var canvas = document.getElementById("canvasDisplay");
-var ctx = canvas.getContext("2d");
-
 // Constants
 var _BUFFER = 15;
 var _WIDTH = 500;
 var _HEIGHT = 500;
 // Variables
 var lst_offers = [];
+
+var _BUYER = false;
 
 class Offer
 {
@@ -23,6 +22,44 @@ class Offer
 	isBuyer() { return this.buyer; }
 	isSeller() { return !this.buyer; }
 
+	drawHTML()
+	{
+		/* OFFER FORMAT
+          <div class='offer-shape buyer'>
+            <div class='offer-internal' style='float: left;'>
+              $ XX
+            </div>
+            <div class='offer-internal' style='float: right;'>
+              <button>Accept</button>
+            </div>
+          </div>
+		 */
+		var html = "<div class='offer-shape ";
+		if(isBuyer()) { html += "buyer"; }
+		html+="'><div class='offer-internal' style='float: left;'>$ ";
+		//html+=string(this.value);
+	html+=string(25);
+		html+="</div><div class='offer-internal' style='float: right;'>";
+		html+="<button ";
+		if(_BUYER == isBuyer()){ html += "disabled"; }
+		html+=">Accept</button></div></div>";
+	}
+}
+
+function draw()
+{
+	var html = "<div class='offer-shape ";
+	if(false) { html += "buyer"; }
+	else { html += "seller"; }
+	html+="'><div class='offer-internal' style='float: left;'>$ ";
+	//html+=string(this.value);
+	html+=String(25);
+	html+="</div><div class='offer-internal' style='float: right;'>";
+	html+="<button ";
+	if(_BUYER == true){ html += "disabled"; }
+	html+=">Accept</button></div></div>";
+
+	document.getElementById("offer-list").innerHTML=html;
 }
 
 function recieveNewOffer(offer)
