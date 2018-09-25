8597343d0b6149a89d60c5891102991670b666a1
thebagleboy <alfred.burgess95@gmail.com>
2018-09-25 15:09:28 +0800

    Converting to transactions

--- views/offers.js
+++ views/offers.js
@@ -12,7 +12,7 @@ class Offer
 	constructor(offer_id, buyer, val, time)
 	{
 		this.offer_id = offer_id;
-		this.buyer = buyer;
+		if(buyer == 0) { this.buyer = false } else { this.buyer = true; }
 		this.val = val;
 		this.time = time;
 		this.drawn = false;
@@ -39,18 +39,25 @@ class Offer
       </div>
 	 */
 
+
+	// Get initial content
+	var old = document.getElementById("offer-list").innerHTML;
+
+
 	// Create HTML element
 	var html = "<div class='offer-shape ";
-	if(this.isBuyer) { html += "buyer"; } else { html += "seller"; }
+	if(this.buyer) { html += "buyer"; } else { html += "seller"; }
 	html+="'><div class='offer-internal' style='float: left;'>$ ";
 	html+=String(this.val);
 	html+="</div><div class='offer-internal' style='float: right;'>";
-	html+="<button ";
-	if(playerBuyer == false){ html += "disabled"; }
+	html+="<button onclick='acceptOffer(";
+	html+=String(this.offer_id);
+	html+=")'' ";
+	if(_BUYER == this.buyer){ html += "disabled"; }
 	html+=">Accept</button></div></div>";
 
 	// Draw to screen
-	document.getElementById("offer-list").innerHTML+=html;
+	document.getElementById("offer-list").innerHTML = html + old;
 
 	// Set as drawn
 	this.drawn = true;
@@ -75,15 +82,23 @@ function test_draw(offerBuyer, playerBuyer)
 	return 0;
 }
 
-function recieveNewOffer(offer_id, buyer, val, time)
-{
-	lst_offers.push(new Offer(offer_id, buyer, val, time))
-}
-
 function drawOfferList()
 {
 	for(var i = 0; i < lst_offers.length; i++)
 	{
 		lst_offers[i].drawHTML();
 	}
+}
+
+
+/* Use these functions to connect to web sockets */
+function recieveNewOffer(offer_id, buyer, val, time)
+{
+	lst_offers.push(new Offer(offer_id, buyer, val, time))
+}
+
+function acceptOffer(offerID)
+{
+	console.log("Accepting Offer...");
+	console.log(String(offerID));
 }
