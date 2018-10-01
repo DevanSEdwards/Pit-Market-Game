ce7e08bea09d6f36ed2e352fff33c21cedcb03cf
21716087 <wezybill4jc@gmail.com>
2018-10-01 21:29:50 +0800

    Added initial cookies implementation

---
+++ public/scripts/state.js
@@ -0,0 +1,24 @@
+export default class State 
+{
+    clientId = null;
+    gameId = null;
+    isHost = null;
+    currentPage = null;
+    deckSetting = {
+        domain: 7,
+        mean: 6,
+        lowerLimit: 2
+    };
+    offers = [];
+    rounds = {
+        settings = {
+            length: 120,
+            offerTimeLimit: 10,
+            tax: null,
+            ceiling: null,
+            floor: null
+        },
+        trade: []            
+    };
+    websocket = null;
+}
