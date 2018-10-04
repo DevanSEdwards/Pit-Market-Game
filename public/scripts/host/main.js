53d79f778e5329c9ce303eaf8d9314a762090b41
21716087 <wezybill4jc@gmail.com>
2018-10-04 13:11:18 +0800

    Javascript host functions

--- public/scripts/host/main.js
+++ public/scripts/host/main.js
@@ -2,4 +2,65 @@
 function main() {
     loadpage(`deckSettings`);
     initDeckSettings();
+
+    state.websocket.onmessage = (event) => {
+        msg = JSON.parse(event.data);
+        console.log(msg);
+
+        switch (msg.type) {
+            case "offer":
+                addOffer(msg, false, ws);
+                break;
+            case "start round":
+                var timeRemaining = msg.length;
+                var countInterval = window.setInterval(() => {
+                    document.getElementById("roundCounter").innerText =
+                        `Time: ${Math.floor(timeRemaining / 60).toString()}:${(timeRemaining % 60).toString().padStart(2, "0")}`;
+                    if (timeRemaining-- == 0)
+                        clearInterval(countInterval);
+                }, 1000)
+                for (var i = 0; i < offerIds.length; i++) {
+                    document.getElementById(offerIds[i]).remove();
+                }
+                offerIds = [];
+                break;
+            case "end game":
+                window.location.href = "/";
+        }
+    };
 }
+
+function addOffer(msg, addAcceptEvent, ws) {
+    var tradeBtn = document.createElement("input");
+    tradeBtn.id = msg.offerId;
+    offerIds.push(msg.offerId);
+    tradeBtn.type = "button";
+    tradeBtn.value = msg.price;
+
+    var fadeTime = 500;
+    window.setTimeout(() => {
+        fadeOut(tradeBtn, fadeTime);
+        window.setTimeout(() => {
+            offerIds.splice(offerIds.indexOf(msg.offerId), 1);
+            tradeBtn.remove();
+        }, fadeTime);
+    }, offerTimeout - fadeTime);
+
+    // Enable btn if appropriate
+    if (addAcceptEvent)
+        tradeBtn.addEventListener("click", () => {
+            ws.send(JSON.stringify({
+                "type": "accept",
+                "offerId": msg.offerId
+            }))
+        })
+    else
+        tradeBtn.disabled = true;
+
+    // Append btn to div
+    if (msg.isSeller)
+        document.getElementById("sellOffers").appendChild(tradeBtn);
+    else
+        document.getElementById("buyOffers").appendChild(tradeBtn);
+}
+
