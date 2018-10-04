83c9e1d5e9773d8b47bce62ba6843aa4446c02a1
Tim Wise <21957423@student.uwa.edu.au>
2018-10-04 17:02:57 +0800

    Commented out breaking code in main

--- public/scripts/host/main.js
+++ public/scripts/host/main.js
@@ -1,66 +1,27 @@
 // Called in setup.js
 function main() {
-    loadpage(`deckSettings`);
     initDeckSettings();
+    loadpage(`deckSettings`);
+    state.websocket.onmessage = handleMessage;
+}
 
-    state.websocket.onmessage = (event) => {
-        msg = JSON.parse(event.data);
+function handleMessage(event) {
+    msg = JSON.parse(event.data);
         console.log(msg);
 
         switch (msg.type) {
-            case "offer":
-                addOffer(msg, false, ws);
+            case `offer`:
                 break;
-            case "start round":
-                var timeRemaining = msg.length;
-                var countInterval = window.setInterval(() => {
-                    document.getElementById("roundCounter").innerText =
-                        `Time: ${Math.floor(timeRemaining / 60).toString()}:${(timeRemaining % 60).toString().padStart(2, "0")}`;
-                    if (timeRemaining-- == 0)
-                        clearInterval(countInterval);
-                }, 1000)
-                for (var i = 0; i < offerIds.length; i++) {
-                    document.getElementById(offerIds[i]).remove();
-                }
-                offerIds = [];
+            case `start round`:
+                loadpage(`round`);
+                state.inRound = true
                 break;
-            case "end game":
-                window.location.href = "/";
+            case `end round`:
+                loadpage(`roundSettings`);
+                state.inRound = false;
+                break;
+            case `end game`:
+                window.location.replace(`/`);
+                break;
         }
-    };
 }
-
-function addOffer(msg, addAcceptEvent, ws) {
-    var tradeBtn = document.createElement("input");
-    tradeBtn.id = msg.offerId;
-    offerIds.push(msg.offerId);
-    tradeBtn.type = "button";
-    tradeBtn.value = msg.price;
-
-    var fadeTime = 500;
-    window.setTimeout(() => {
-        fadeOut(tradeBtn, fadeTime);
-        window.setTimeout(() => {
-            offerIds.splice(offerIds.indexOf(msg.offerId), 1);
-            tradeBtn.remove();
-        }, fadeTime);
-    }, offerTimeout - fadeTime);
-
-    // Enable btn if appropriate
-    if (addAcceptEvent)
-        tradeBtn.addEventListener("click", () => {
-            ws.send(JSON.stringify({
-                "type": "accept",
-                "offerId": msg.offerId
-            }))
-        })
-    else
-        tradeBtn.disabled = true;
-
-    // Append btn to div
-    if (msg.isSeller)
-        document.getElementById("sellOffers").appendChild(tradeBtn);
-    else
-        document.getElementById("buyOffers").appendChild(tradeBtn);
-}
-
--- public/scripts/player/main.js
+++ public/scripts/player/main.js
@@ -1,57 +1,52 @@
 // Called in setup.js
 function main() {
     loadpage(`lobby`);
-
-    state.websocket.onmessage = (event) => {
-        msg = JSON.parse(event.data);
-        console.log(msg);
-
-        switch (msg.type) {
-            case "card":
-                document.getElementById("isSeller").innerText = msg.isSeller ? "Seller" : "Buyer";
-                document.getElementById("cardValue").innerText = `Card: ${msg.value}`;
-                isSeller = msg.isSeller;
-                if (isSeller)
-                {
-                    document.getElementById("card").innerText = "Selling";
-                }
-                else
-                {
-                    document.getElementById("card").innerText = "Buying";
-                }
-                
-                cardValue = msg.value;
-                draw();
-                break;
-            case "offer":
-                addOffer(msg, msg.isSeller ? !isSeller : isSeller, ws);
-                break;
-            case "start round":
-                offerTimeout = msg["offer time limit"] * 1000;
-                var timeRemaining = msg.length;
-                var countInterval = window.setInterval(() => {
-                    document.getElementById("roundCounter").innerText =
-                        `Time: ${Math.floor(timeRemaining / 60).toString()}:${(timeRemaining % 60).toString().padStart(2, "0")}`;
-                    if (timeRemaining-- == 0)
-                        clearInterval(countInterval);
-                }, 1000)
-                for (var i = 0; i < offerIds.length; i++) {
-                    document.getElementById(offerIds[i]).remove();
-                }
-                offerIds = [];
-                break;
-            case "end game":
-                window.location.href = "/";
-            case "trade":
-                if (msg.success) {
-                    document.getElementById("btnPostOffer").disabled = true;
-                    document.getElementById("btnPostOffer").value = `Traded at: ${msg.price.toString()}`;
-                }
-            case "announce trade":
-                var announce = document.createElement("p");
-                announce.innerHTML = `Successful Trade: ${msg.price.toString()}`;
-                var tradeList = document.getElementById("tradeList");
-                tradeList.appendChild(announce);
-        }
-    };
+    state.websocket.onmessage = handleMessage;
 }
+
+function handleMessage(event) {
+    msg = JSON.parse(event.data);
+    console.log(msg);
+
+    switch (msg.type) {
+        case `card`:
+            // document.getElementById(`isSeller`).innerText = msg.isSeller ? `Seller` : `Buyer`;
+            // document.getElementById(`cardValue`).innerText = `Card: ${msg.value}`;
+            // isSeller = msg.isSeller;
+            // if (isSeller) {
+            //     document.getElementById(`card`).innerText = `Selling`;
+            // }
+            // else {
+            //     document.getElementById(`card`).innerText = `Buying`;
+            // }
+
+            // cardValue = msg.value;
+            // draw();
+            break;
+        case `offer`:
+            break;
+        case `trade`:
+            // if (msg.success) {
+            //     document.getElementById(`btnPostOffer`).disabled = true;
+            //     document.getElementById(`btnPostOffer`).value = `Traded at: ${msg.price.toString()}`;
+            // }
+            break;
+        case `announce trade`:
+            // var announce = document.createElement(`p`);
+            // announce.innerHTML = `Successful Trade: ${msg.price.toString()}`;
+            // var tradeList = document.getElementById(`tradeList`);
+            // tradeList.appendChild(announce);
+            break;
+        case `start round`:
+            loadpage(`round`);
+            state.inRound = true
+            break;
+        case `end round`:
+            loadpage(`lobby`);
+            state.inRound = false;
+            break;
+        case `end game`:
+            window.location.replace(`/`);
+            break;
+    }
+};
