bf8de8cf11f949ea4b6a20fa5b9e3e55a4fe33d5
thebagleboy <alfred.burgess95@gmail.com>
2018-10-11 10:48:23 +0800

    Trying to merge front end in. layout.css | used by lobby.html time.js | used for timer element transactions.js | same as offers.js
    round.html should have timer
    lobby.html replaced by front-end version

---
+++ public/scripts/time.js
@@ -0,0 +1,67 @@
+// Blocks
+var lst_blocks = []; // Queue of blocked IDs  // { expire | id }
+
+
+function shiftBlocks()
+{
+  // Checks if the front of the queue is expired
+  // If expired, will unblock the element by ID
+  if(lst_blocks.length < 1) { return; }
+  var now = Date.now();
+  while(1)
+  {
+    if(lst_blocks[0][1] >= now)
+    {
+      var block = lst_blocks.shift();
+      unblockID (block);
+    } else { return; }
+  } return;
+}
+
+// Removes disabled attribute from ID
+function unblockID(id) { document.getElementById(id).removeAttribute("disabled"); }
+// Add's disabled attribute to ID and adds to queue
+function blockID(id, time)
+{
+  var t = Date.now();
+  var e = t + (time * 1000);
+
+  lst_blocks.push([time, id]);
+  document.getElementById(id).setAttribute("disabled", true);
+  // TODO: Fix this
+}
+
+
+/* Timer Functions
+ */
+
+// Timer Constant
+var _currentTimer = 120; // In seconds
+
+// ToString() wrappers
+function secondsToString(s) { var ret = s2m(s); return String( ret[0] + ":" + ret[1] ); }
+function minutesToString(m,s) { return String( m2s(m,s) ); }
+
+// Conerts seconds to minutes
+function s2m(t)
+{
+  var m = parseInt(t / 60); var s = t % 60;
+  m = (m < 10) ? "0" + String(m) : String(m);
+  s = (s < 10) ? "0" + String(s) : String(s);
+  return [m, s];
+}
+// Convert Minutes to seconds
+function m2s(m,s) { return (m * 60) + s; }
+// set timer constant to time (in seconds)
+function setTimer_s(t){_currentTimer = t; timeDisplay(s2m(_currentTimer));}
+// set timer constant to time (in minutes,seconds)
+function setTimer_ms(m,s){_currentTimer = m2s(m,s); timeDisplay(s2m(_currentTimer));}
+
+// Sets the element to the time
+function setTimerDisplay(element, t) { document.getElementById(element).innerHTML = "<span>Time: </span>" + t;}
+function incrementTimer()
+{ // Increment timer
+  if(_currentTimer >= 0) { return; } // Don't go negative
+  setTimerDisplay("info_roundCounter", secondsToString(--_currentTimer));
+}
+
