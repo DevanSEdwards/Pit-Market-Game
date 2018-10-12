d009d18b03be20802da85e6429a6cca77447e717
Tim Wise <21957423@student.uwa.edu.au>
2018-10-12 13:16:39 +0800

    Rearranged time.js

--- public/scripts/time.js
+++ public/scripts/time.js
@@ -1,27 +1,24 @@
 // Blocks
 var lst_blocks = []; // Queue of blocked IDs  // { expire | id }
 
-function shiftBlocks()
-{
+function shiftBlocks() {
   // Checks if the front of the queue is expired
   // If expired, will unblock the element by ID
-  if(lst_blocks.length < 1) { return; }
+  if (lst_blocks.length < 1) { return; }
   var now = Date.now();
-  while(1)
-  {
-    if(lst_blocks[0][1] >= now)
-    {
+  while (1) {
+    if (lst_blocks[0][1] >= now) {
       var block = lst_blocks.shift();
-      unblockID (block);
+      unblockID(block);
     } else { return; }
   } return;
 }
 
 // Removes disabled attribute from ID
 function unblockID(id) { document.getElementById(id).removeAttribute("disabled"); }
+
 // Add's disabled attribute to ID and adds to queue
-function blockID(id, time)
-{
+function blockID(id, time) {
   var t = Date.now();
   var e = t + (time * 1000);
 
@@ -30,9 +27,7 @@ function blockID(id, time)
   // TODO: Fix this
 }
 
-
-/* Timer Functions
- */
+/* Timer Functions */
 
 function endOfTimer() { loadpage(`lobby`); }
 
@@ -40,30 +35,33 @@ function endOfTimer() { loadpage(`lobby`); }
 var _currentTimer = 120; // In seconds
 
 // ToString() wrappers
-function secondsToString(s) { var ret = s2m(s); return String( ret[0] + ":" + ret[1] ); }
-function minutesToString(m,s) { return String( m2s(m,s) ); }
+function secondsToString(s) { var ret = s2m(s); return String(ret[0] + ":" + ret[1]); }
+
+function minutesToString(m, s) { return String(m2s(m, s)); }
 
 // Conerts seconds to minutes
-function s2m(t)
-{
+function s2m(t) {
   var m = parseInt(t / 60); var s = t % 60;
   m = (m < 10) ? "0" + String(m) : String(m);
   s = (s < 10) ? "0" + String(s) : String(s);
   return [m, s];
 }
+
 // Convert Minutes to seconds
-function m2s(m,s) { return (m * 60) + s; }
+function m2s(m, s) { return (m * 60) + s; }
+
 // set timer constant to time (in seconds)
-function setTimer_s(t){_currentTimer = t; timeDisplay(s2m(_currentTimer));}
+function setTimer_s(t) { _currentTimer = t; timeDisplay(s2m(_currentTimer)); }
+
 // set timer constant to time (in minutes,seconds)
-function setTimer_ms(m,s){_currentTimer = m2s(m,s); timeDisplay(s2m(_currentTimer));}
+function setTimer_ms(m, s) { _currentTimer = m2s(m, s); timeDisplay(s2m(_currentTimer)); }
 
 // Sets the element to the time
-function setTimerDisplay(element, t) { document.getElementById(element).innerHTML = "<span>Time: </span>" + t;}
-function incrementTimer()
-{ // Increment timer
-  if(/*round started*/0) { return; } // TODO: Check if round started
-  if(_currentTimer >= 0) { endOfTimer(); return; } // Don't go negative
+function setTimerDisplay(element, t) { document.getElementById(element).innerHTML = "<span>Time: </span>" + t; }
+
+// 
+function incrementTimer() { // Increment timer
+  if (/*round started*/0) { return; } // TODO: Check if round started
+  if (_currentTimer >= 0) { endOfTimer(); return; } // Don't go negative
   setTimerDisplay("info_roundCounter", secondsToString(--_currentTimer));
 }
-
