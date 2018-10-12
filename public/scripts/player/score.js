42c59fee61919ae82c59ca2309eeee0d9c1a6ccd
Tim Wise <21957423@student.uwa.edu.au>
2018-10-12 15:40:57 +0800

    .

--- public/scripts/player/score.js
+++ public/scripts/player/score.js
@@ -2,7 +2,11 @@ function calculateScore()
 {
 	var rounds = state.rounds;
 	var profit = 0;
+<<<<<<< Updated upstream
 	for(var i = 0; i < rounds.length; i++)
+=======
+	for(let i = 0; i < rounds.length; i++)
+>>>>>>> Stashed changes
 	{
 		var price = round[i].tradePrice;
 		if(price = null) { continue; }
