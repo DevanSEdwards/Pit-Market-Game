11053bc73203e9c291fffacd9c4f25e8538878d9
thebagleboy <alfred.burgess95@gmail.com>
2018-10-12 13:28:14 +0800

    changed transactions.js to match with offer.js

--- public/scripts/transactions.js
+++ public/scripts/transactions.js
@@ -1,84 +1,31 @@
-// Constants
-var _BUFFER = 15;
-var _WIDTH = 500;
-var _HEIGHT = 500;
-// Variables
-var lst_transactions = [];
-
-class Transaction
+function Transaction(transactionID, value, time)
 {
-	constructor(transaction_id, val, time)
-	{
-		this.transaction_id = transaction_id;
-		this.val = val;
-		this.time = time;
-		this.drawn = false;
-	}
-	// Getters
-	transaction_id() { return this.transaction_id; }
-	val() { return this.val; }
-	time() { return this.time; }
-
-	// Methods
-	drawHTML()
-	{
-	if(this.drawn) { /*Don't draw it again */ return 1; }
-	/* OFFER FORMAT
-      <div class='offer-shape buyer'>
-        <div class='offer-internal' style='float: left;'>
-          $ XX\
-        </div>
-        <div class='offer-internal' style='float: right;'>
-          <button>Accept</button>
-        </div>
-      </div>
-	 */
-
-
-	// Get initial content
-	var old = document.getElementById("trade-list").innerHTML;
-
-
-	// Create HTML element
-	/*
-
-      <div class='offer-shape trade'>
-        <div class='offer-internal' style='float: left;'>
-          $ XX\
-        </div>
-        <div class='offer-internal' style='float: right;'>
-          <button>Accept</button>
-        </div>
-      </div>
-
-	*/
-
-	var html = "<div class='offer-shape trade'><div class='offer-internal' style='float: left;'>";
-	html+= String(this.val);
-	html+= "</div><div class='offer-internal' style='float: right;'>";
-	html+= String(this.time);
-	html+="</div></div>";
-
-	// Draw to screen
-	document.getElementById("trade-list").innerHTML = html + old;
-
-	// Set as drawn
-	this.drawn = true;
-	return 0;
-	}
+	this.transactionID = transactionID;
+	this.price = value;
+	this.time = time;
 }
 
-function drawTransactionsList()
+function drawTransactionList()
 {
-	for(var i = 0; i < lst_transactions.length; i++)
-	{
-		lst_transactions[i].drawHTML();
-	}
+	// TODO: Don't redraw every element everytime
+	document.getElementById("offer-list").innerHTML = state.offers
+		.map(offer => `
+			<div class='offer-shape trade'>
+				<div class="offer-internal" style="float: left">
+				$ ${String(transaction.price)}
+				</div>
+				<div class="offer-internal" style="float: right">
+				$ ${String(transaction.time)}
+				</div>
+			</div>
+			`);
+		.join(``);
 }
 
-
-/* Use these functions to connect to web sockets */
-function recieveNewTrade(transaction_id, val, time)
-{
-	lst_transactions.push(new Transaction(transaction_id, val, time))
+function recieveNewTransaction(transactionID, price, time) {
+	// Called in main.js when a message of type 'transaction' is received
+	// isSeller is whether the offer is to sell or not
+	state.offers.push(new Transaction(transactionID, price, time));
+	drawTransactionList();
 }
+
