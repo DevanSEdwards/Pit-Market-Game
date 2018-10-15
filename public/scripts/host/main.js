b6078c5e5da046377d2f663d20b66e795247f919
Tim Wise <21957423@student.uwa.edu.au>
2018-10-15 12:00:09 +0800

    Merge branch 'graph' into test-merge

--- public/scripts/host/main.js
+++ public/scripts/host/main.js
@@ -32,7 +32,7 @@ function handleMessage(event) {
             state.inRound = false;
             break;
         case `end game`:
-            window.location.replace(`/`);
+            loadpage(`endGame`);
             break;
     }
 }
